826c9e7b21535a71feaad4bda1a48a0e84276ecb
adevreactjs <adevreactjs@gmail.com>
2021-05-24 23:53:50 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -4,6 +4,19 @@ $(function(){
     dots: true,
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    responsive: [{
+      breakpoint: 841,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 601,
+      settings: {
+        slidesToShow: 1,
+      }
+    }
+    ]
  });
 });
